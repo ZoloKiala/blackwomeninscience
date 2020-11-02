b4c861c02ccb2d0b1c17fadfaa4d608b109dbff9
User <User@0371261-PPO.local.ukzn.ac.za>
2020-11-03 00:12:44 +0200

    edits on settings

--- bws/settings.py
+++ bws/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(x7=qwjx^6_-0#e=**$$t1%$*%#iw646f-3n_58783_yqog9%#'
+SECRET_KEY = 'x7=qwjx^6_-0#e=**$$t1%$*%#iw646f-3n_58783_yqog9%#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
