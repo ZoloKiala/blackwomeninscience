fc0b6a6e909ef97c628010f7e74a6aa65cd11d7c
User <User@0371261-PPO.local.ukzn.ac.za>
2020-11-04 09:13:39 +0200

    edits on settings

--- bws/settings.py
+++ bws/settings.py
@@ -125,14 +125,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
 
 
 MEDIA_URL = '/media/'
