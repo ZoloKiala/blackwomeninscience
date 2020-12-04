374bbf0906ec621142219a37642b432ff566b34f
User <User@0371261-PPO.local.ukzn.ac.za>
2020-12-04 14:47:58 +0200

    changes

--- bws/settings.py
+++ bws/settings.py
@@ -157,7 +157,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 AWS_LOCATION = 'static'
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
-MEDIA_URL = '//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_URL = '//%s.s3.amazonaws.com/media/pics' % AWS_STORAGE_BUCKET_NAME
 MEDIA_ROOT = MEDIA_URL
 
 EMAIL_HOST = 'smtp.gmail.com'
