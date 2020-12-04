c86337923edb573b736988e888801094c74b9853
User <User@0371261-PPO.local.ukzn.ac.za>
2020-12-04 15:04:50 +0200

    changes

--- bws/settings.py
+++ bws/settings.py
@@ -156,7 +156,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 AWS_LOCATION = 'static'
 
-MEDIA_URL = '//%s.s3.amazonaws.com/media/pics' % AWS_STORAGE_BUCKET_NAME
+MEDIA_URL = '//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
 MEDIA_ROOT = MEDIA_URL
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
