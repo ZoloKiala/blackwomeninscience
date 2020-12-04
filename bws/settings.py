af9c003cb83de26483cf456a1c4b4d4b9bd8628b
User <User@0371261-PPO.local.ukzn.ac.za>
2020-12-04 12:47:34 +0200

    changes

--- bws/settings.py
+++ bws/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
     'bwise'
 ]
 
@@ -146,23 +147,22 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_ACCESS_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
 
-
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_HOST_USER = '587'
-# EMAIL_HOST_PASSWORD = 'zolokiala@gmail.com'
-# EMAIL_PORT = ''
-# EMAIL_USE_TLS = True
 
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
-EMAIL_HOST_USER = 'zolokiala@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
 
-
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
 
