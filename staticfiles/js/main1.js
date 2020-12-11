176b8ada1b83b3422ab194c39d2cab2fe824647b
User <User@0371261-PPO.local.ukzn.ac.za>
2020-12-11 07:54:27 +0200

    changes

--- staticfiles/js/main1.js
+++ staticfiles/js/main1.js
@@ -1,4 +1,33 @@
 
+        // Clicking dropdown button will toggle display 
+        function btnToggle() { 
+          document.getElementById("Dropdown").classList.toggle("show"); 
+      } 
+        
+      // Prevents menu from closing when clicked inside 
+      document.getElementById("Dropdown").addEventListener('click', function (event) { 
+          alert("click outside"); 
+          event.stopPropagation(); 
+      }); 
+        
+      // Closes the menu in the event of outside click 
+      window.onclick = function(event) { 
+          if (!event.target.matches('.dropbutton')) { 
+            
+              var dropdowns =  
+              document.getElementsByClassName("dropdownmenu-content"); 
+                
+              var i; 
+              for (i = 0; i < dropdowns.length; i++) { 
+                  var openDropdown = dropdowns[i]; 
+                  if (openDropdown.classList.contains('show')) { 
+                      openDropdown.classList.remove('show'); 
+                  } 
+              } 
+          } 
+      } 
+     
+
 function myFunction() {
   var dots = document.getElementById("dots");
   var moreText = document.getElementById("more");
@@ -77,5 +106,7 @@ function myFunction1() {
 // $('h4.team-member-name').on('mouseenter', function(){
 //   $(this).toggleClass('h4.team-member-name1')
 // })
+
+
 
   
